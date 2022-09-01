34e2cb361e627ff43e399eea415bebcf151fc8bb
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2022-09-01 10:45:53 +0200

    test(tui): get_row_inside works

--- src/interactive/ui.rs
+++ src/interactive/ui.rs
@@ -16,11 +16,33 @@ pub const fn focus_color(has_focus: bool) -> Color {
     }
 }
 
-/// When the column/row is inside the area, return the row relative to the area. Otherwise `None` is returned.
+/// When the column/row is inside the area, return the row relative to the area.
+/// Otherwise `None` is returned.
 pub fn get_row_inside(area: Rect, column: u16, row: u16) -> Option<u16> {
     if row > area.top() && row < area.bottom() && column > area.left() && column < area.right() {
         Some(row - area.top() - 1)
     } else {
         None
     }
+}
+
+#[test]
+fn row_outside() {
+    let area = Rect::new(5, 5, 5, 10);
+    let result = get_row_inside(area, 7, 1);
+    assert_eq!(result, None);
+}
+
+#[test]
+fn column_outside() {
+    let area = Rect::new(5, 5, 5, 10);
+    let result = get_row_inside(area, 1, 7);
+    assert_eq!(result, None);
+}
+
+#[test]
+fn is_inside() {
+    let area = Rect::new(5, 5, 5, 10);
+    let result = get_row_inside(area, 7, 10);
+    assert_eq!(result, Some(4));
 }
