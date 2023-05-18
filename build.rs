e3a55c9057827684c89d3abc7ecc7a7a52efc9ec
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-05-18 15:23:01 +0200

    build(clap): use Result instead of unwrap

--- build.rs
+++ build.rs
@@ -3,19 +3,21 @@ use clap_complete::{generate_to, shells};
 
 include!("src/cli.rs");
 
-fn main() {
+fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cli.rs");
 
     let out_dir = std::path::Path::new("target/completions/");
     drop(std::fs::remove_dir_all(out_dir));
-    std::fs::create_dir_all(out_dir).unwrap();
+    std::fs::create_dir_all(out_dir)?;
 
     let bin_name = env!("CARGO_PKG_NAME");
 
-    generate_to(shells::Bash, &mut Cli::command(), bin_name, out_dir).unwrap();
-    generate_to(shells::Elvish, &mut Cli::command(), bin_name, out_dir).unwrap();
-    generate_to(shells::Fish, &mut Cli::command(), bin_name, out_dir).unwrap();
-    generate_to(shells::PowerShell, &mut Cli::command(), bin_name, out_dir).unwrap();
-    generate_to(shells::Zsh, &mut Cli::command(), bin_name, out_dir).unwrap();
+    generate_to(shells::Bash, &mut Cli::command(), bin_name, out_dir)?;
+    generate_to(shells::Elvish, &mut Cli::command(), bin_name, out_dir)?;
+    generate_to(shells::Fish, &mut Cli::command(), bin_name, out_dir)?;
+    generate_to(shells::PowerShell, &mut Cli::command(), bin_name, out_dir)?;
+    generate_to(shells::Zsh, &mut Cli::command(), bin_name, out_dir)?;
+
+    Ok(())
 }
