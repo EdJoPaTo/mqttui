58620bb22ee46a747f571533abe8072beff6acae
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-19 18:29:54 +0100

    refactor(mqtt): move Time enum into its own file

--- src/mqtt/mod.rs
+++ src/mqtt/mod.rs
@@ -1,6 +1,8 @@
 pub mod encryption;
 mod history_entry;
 mod payload;
+mod time;
 
-pub use history_entry::*;
+pub use history_entry::HistoryEntry;
 pub use payload::Payload;
+pub use time::Time;
---
+++ src/mqtt/time.rs
@@ -0,0 +1,56 @@
+use chrono::NaiveDateTime;
+
+#[derive(Debug, Clone, Copy)]
+pub enum Time {
+    Retained,
+    Local(NaiveDateTime),
+}
+
+impl Time {
+    pub fn new_now(retain: bool) -> Self {
+        if retain {
+            Self::Retained
+        } else {
+            Self::Local(chrono::Local::now().naive_local())
+        }
+    }
+
+    pub const fn as_optional(&self) -> Option<&NaiveDateTime> {
+        if let Self::Local(time) = self {
+            Some(time)
+        } else {
+            None
+        }
+    }
+}
+
+impl ToString for Time {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Retained => String::from("RETAINED"),
+            Self::Local(time) => time.format("%_H:%M:%S.%3f").to_string(),
+        }
+    }
+}
+
+#[test]
+fn optional_retained() {
+    let time = Time::Retained;
+    assert_eq!(time.as_optional(), None);
+}
+
+#[test]
+fn optional_time() {
+    let date = chrono::NaiveDate::from_ymd_opt(1996, 12, 19)
+        .unwrap()
+        .and_hms_opt(16, 39, 57)
+        .unwrap();
+    let time = Time::Local(date);
+    assert_eq!(time.as_optional(), Some(&date));
+}
+
+#[test]
+fn retained_to_string() {
+    let time = Time::Retained;
+    assert_eq!(time.to_string(), "RETAINED");
+}
