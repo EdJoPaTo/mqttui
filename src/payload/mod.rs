576d82cf7e0d427bdd64eb58202b872231a0d911
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-20 21:35:06 +0100

    test: has_duplicate_keys works

--- src/payload/mod.rs
+++ src/payload/mod.rs
@@ -1,4 +1,5 @@
 mod json_selector;
+mod messagepack;
 mod tree_items_from_json;
 mod tree_items_from_messagepack;
 
@@ -20,7 +21,7 @@ impl Payload {
             Ok(str) => {
                 serde_json::from_str(&str).map_or_else(|_| Self::String(str.into()), Self::Json)
             }
-            Err(err) => decode_messagepack(err.as_bytes())
+            Err(err) => messagepack::decode(err.as_bytes())
                 .map_or_else(|| Self::NotUtf8(err.utf8_error()), Self::MessagePack),
         }
     }
@@ -33,54 +34,6 @@ impl Payload {
             Self::String(str) => format!("Payload({size:>3}): {str}"),
         }
     }
-}
-
-/// Attempts to decode [`MessagePack`](rmpv::Value) from the payload.
-/// Tries to find out if data seems valid.
-pub fn decode_messagepack(mut payload: &[u8]) -> Option<rmpv::Value> {
-    fn has_duplicate_keys(value: &rmpv::Value) -> bool {
-        match value {
-            rmpv::Value::Nil
-            | rmpv::Value::Boolean(_)
-            | rmpv::Value::Integer(_)
-            | rmpv::Value::F32(_)
-            | rmpv::Value::F64(_)
-            | rmpv::Value::String(_)
-            | rmpv::Value::Binary(_)
-            | rmpv::Value::Ext(_, _) => false,
-            rmpv::Value::Array(values) => values.iter().any(has_duplicate_keys),
-            rmpv::Value::Map(map) => {
-                let mut keys = map
-                    .iter()
-                    .map(|(key, _value)| {
-                        key.as_str()
-                            .map_or_else(|| key.to_string(), ToOwned::to_owned)
-                    })
-                    .collect::<Vec<_>>();
-                let before = keys.len();
-                keys.sort_unstable();
-                keys.dedup();
-                if before > keys.len() {
-                    return true;
-                }
-                map.iter()
-                    .map(|(_key, value)| value)
-                    .any(has_duplicate_keys)
-            }
-        }
-    }
-
-    let value = rmpv::decode::read_value(&mut payload).ok()?;
-    if value.to_string().len() < payload.len() {
-        // The JSON should be bigger than the bytes.
-        // Otherwise there is data missing from the bytes so its unlikely MessagePack.
-        return None;
-    }
-    if has_duplicate_keys(&value) {
-        // Can not be displayed by the Tree widget currently anyway
-        return None;
-    }
-    Some(value)
 }
 
 #[test]
