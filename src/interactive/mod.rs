05bbbcbd4b95728bc059c1d2573d5f80bbaa6d5d
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-24 15:46:49 +0100

    perf(interactive): check events on main loop
    Dont need to send them to another thread which is only doing that anyway.

--- src/interactive/mod.rs
+++ src/interactive/mod.rs
@@ -1,10 +1,6 @@
-use std::sync::mpsc::{self, Receiver};
-use std::thread;
 use std::time::{Duration, Instant};
 
-use crossterm::event::{
-    Event as CEvent, KeyCode, KeyEvent, KeyModifiers, MouseButton, MouseEventKind,
-};
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers, MouseButton, MouseEventKind};
 use ratatui::backend::Backend;
 use ratatui::layout::{Alignment, Rect};
 use ratatui::text::Span;
@@ -26,15 +22,6 @@ mod mqtt_history;
 mod mqtt_thread;
 mod topic_overview;
 mod ui;
-
-enum Event {
-    Key(KeyEvent),
-    MouseClick { column: u16, row: u16 },
-    MouseScrollDown,
-    MouseScrollUp,
-    Resize,
-    Tick,
-}
 
 enum Refresh {
     /// Update the TUI
@@ -90,98 +77,60 @@ pub fn show(
     )?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
 
-    // Setup input handling
-    let (tx, rx) = mpsc::channel();
-
-    thread::spawn(move || {
-        const TICK_RATE: Duration = Duration::from_millis(500);
-        const DEBOUNCE: Duration = Duration::from_millis(20); // 50 FPS
-
-        let mut last_send = Instant::now();
-        let mut last_debounced: Option<Instant> = None;
-        let mut events = Vec::new();
-        'eventloop: loop {
-            let timeout =
-                last_debounced.map_or(DEBOUNCE, |last| DEBOUNCE.saturating_sub(last.elapsed()));
-            if crossterm::event::poll(timeout).unwrap() {
-                let event = match crossterm::event::read().unwrap() {
-                    CEvent::Key(key) => Some(Event::Key(key)),
-                    CEvent::Mouse(mouse) => match mouse.kind {
-                        MouseEventKind::ScrollUp => Some(Event::MouseScrollUp),
-                        MouseEventKind::ScrollDown => Some(Event::MouseScrollDown),
-                        MouseEventKind::Down(MouseButton::Left) => Some(Event::MouseClick {
-                            column: mouse.column,
-                            row: mouse.row,
-                        }),
-                        _ => None,
-                    },
-                    CEvent::Resize(_, _) => Some(Event::Resize),
-                    CEvent::FocusGained | CEvent::FocusLost | CEvent::Paste(_) => None,
-                };
-                if let Some(event) = event {
-                    last_debounced.get_or_insert_with(Instant::now);
-                    events.push(event);
-                }
-            }
-            if last_debounced.map_or_else(
-                || last_send.elapsed() >= TICK_RATE,
-                |last_debounced| last_debounced.elapsed() >= DEBOUNCE,
-            ) {
-                if events.is_empty() {
-                    events.push(Event::Tick);
-                }
-                if tx.send(events).is_err() {
-                    // The receiver is gone → the main thread is finished.
-                    // Just end the loop here, reporting this error is not helpful in any form.
-                    // If the main loop exited successfully this is planned. If not we cant give a helpful error message here anyway.
-                    break 'eventloop;
-                }
-                events = Vec::new();
-                last_send = Instant::now();
-                last_debounced = None;
-            }
-        }
-    });
-
     terminal.clear()?;
 
-    let main_loop_result = main_loop(&mut app, &rx, &mut terminal);
+    let main_loop_result = main_loop(&mut app, &mut terminal);
 
     reset_terminal()?;
 
     main_loop_result
 }
 
-fn main_loop<B>(
-    app: &mut App,
-    rx: &Receiver<Vec<Event>>,
-    terminal: &mut Terminal<B>,
-) -> anyhow::Result<()>
+fn main_loop<B>(app: &mut App, terminal: &mut Terminal<B>) -> anyhow::Result<()>
 where
     B: Backend,
 {
+    const INTERVAL: Duration = Duration::from_millis(500);
+    const DEBOUNCE: Duration = Duration::from_millis(20); // 50 FPS
+
     terminal.draw(|frame| app.draw(frame))?;
-    'main_loop: loop {
-        let mut update = false;
-        for event in rx.recv()? {
-            let refresh = match event {
-                Event::Key(event) => app.on_key(event)?,
-                Event::MouseClick { column, row } => app.on_click(column, row),
-                Event::MouseScrollDown => app.on_scroll_down(),
-                Event::MouseScrollUp => app.on_scroll_up(),
-                Event::Resize | Event::Tick => Refresh::Update,
+
+    let mut last_render = Instant::now();
+    let mut debounce: Option<Instant> = None;
+
+    loop {
+        let timeout = debounce.map_or(INTERVAL, |start| DEBOUNCE.saturating_sub(start.elapsed()));
+        if crossterm::event::poll(timeout)? {
+            let refresh = match crossterm::event::read()? {
+                Event::Key(key) => app.on_key(key)?,
+                Event::Mouse(mouse) => match mouse.kind {
+                    MouseEventKind::Down(MouseButton::Left) => {
+                        app.on_click(mouse.column, mouse.row)
+                    }
+                    MouseEventKind::ScrollDown => app.on_scroll_down(),
+                    MouseEventKind::ScrollUp => app.on_scroll_up(),
+                    _ => Refresh::Skip,
+                },
+                Event::Resize(_, _) => Refresh::Update,
+                Event::FocusGained | Event::FocusLost | Event::Paste(_) => Refresh::Skip,
             };
             match refresh {
-                Refresh::Update => update = true,
+                Refresh::Quit => return Ok(()),
                 Refresh::Skip => {}
-                Refresh::Quit => break 'main_loop,
+                Refresh::Update => {
+                    debounce.get_or_insert_with(Instant::now);
+                }
             }
         }
-        if update {
+        if debounce.map_or_else(
+            || last_render.elapsed() > INTERVAL,
+            |debounce| debounce.elapsed() > DEBOUNCE,
+        ) {
             terminal.draw(|frame| app.draw(frame))?;
+            last_render = Instant::now();
+            debounce = None;
         }
     }
-    Ok(())
 }
 
 pub struct App {
