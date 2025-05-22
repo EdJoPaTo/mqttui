b9cd774fc032729a56088d1f7919aafb9aff6526
Alex Gustafsson <alexg@axis.com>
2025-05-22 08:47:43 +0200

    Add support for specifying CA cert

--- src/cli.rs
+++ src/cli.rs
@@ -243,6 +243,20 @@ pub struct MqttConnection {
     )]
     pub client_key: Option<std::path::PathBuf>,
 
+    /// Path to the TLS CA certificate file.
+    ///
+    /// Used together with --client-cert, --client-key to enable TLS client
+    /// authentication.
+    /// The file has to be a DER-encoded X.509 certificate serialized to PEM.
+    #[arg(
+        long,
+        env = "MQTTUI_CA_CERTIFICATE",
+        value_hint = ValueHint::FilePath,
+        value_name = "FILEPATH",
+        global = true,
+    )]
+    pub ca_cert: Option<std::path::PathBuf>,
+
     /// Allow insecure TLS connections
     #[arg(long, global = true)]
     pub insecure: bool,
